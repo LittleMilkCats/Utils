8d208f164e3a95ece5473bb4ec2b0448899cd284
cat <littlemilkcats@163.com>
2022-03-13 01:07:51 +0800

    这是call方法

---
+++ utils/call.js
@@ -0,0 +1,10 @@
+Function.prototype.call = function(origin,...arguments){
+    if(origin == null || origin == undefined){
+        origin = window
+    }
+    origin.fn = this;
+    console.log(this)
+    let result = origin.fn(...arguments)
+    delete origin.fn
+    return result
+}
