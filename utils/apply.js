864729c44eab12bd2a22d52645b15dd36d1a2196
cat <littlemilkcats@163.com>
2022-03-13 01:37:51 +0800

    这是apply方法

--- utils/apply.js
+++ utils/apply.js
@@ -1,7 +1,7 @@
 Function.prototype.apply = function(origin,arguments){
-    origin == null || undefined ? origin = window : origin
+    origin == null || undefined ? origin = window : origin;
     origin.fn = this;
-    var result = origin.fn(...arguments)
+    var result = origin.fn(...arguments);
     delete origin.fn
     return result
 }
