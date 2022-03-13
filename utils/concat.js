78247a239773f09fa1fa92b821f51ad4054e82a2
cat <1474644435@qq.com>
2022-03-13 12:31:32 +0800

    这是concat方法

---
+++ utils/concat.js
@@ -0,0 +1,11 @@
+Arrar.prototype.concat = function(...args){
+    var arr =[...this];
+    args.forEach(item => {
+        if(Array.isArray(item)){
+            arr.push(...item)
+        }else{
+            arr.push(item)
+        }
+    })
+    return arr
+}
